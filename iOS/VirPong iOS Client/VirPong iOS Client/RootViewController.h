0ea7898c503baa973d0057b92e38eb9b98982967
Josef Lange <josef.d.lange@gmail.com>
2011-09-25 22:42:26 -0700

    Added iOS Client source to iOS directory.
    That about sums it up. This is, however, mostly auto-generated code
    from Xcode.

---
+++ iOS/VirPong iOS Client/VirPong iOS Client/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  VirPong iOS Client
+//
+//  Created by Josef Lange on 9/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ iOS/VirPong iOS Client/VirPong iOS Client/VirPong_iOS_ClientAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  VirPong_iOS_ClientAppDelegate.h
+//  VirPong iOS Client
+//
+//  Created by Josef Lange on 9/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VirPong_iOS_ClientAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
