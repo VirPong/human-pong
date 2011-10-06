2d7d4dbad273deaf2ad38b79a72daedb0411056b
Josef Lange <josef.d.lange@gmail.com>
2011-10-05 19:43:49 -0700

    Maybe implemented connection to server through WebSockets?
    Look at gameIndex.html.

--- iOS/PhoneGapClient/www/PlayGame/game.js
+++ iOS/PhoneGapClient/www/PlayGame/game.js
@@ -1,4 +1,14 @@
  function promptLogin(){
 		var name = prompt("Hey you!"); //"Username please. (Use \"guest\" if you don't already have an account.");
 		//var pass = prompt("Please enter your password. (If you are logging in as \"guest\" then please use \"pass\".)");
+        
  }
+
+function connectToGame() {
+    
+    var mySocket = new GapSocket(cs340-serv, 8000);
+    mySocket.onopen = function(){alert("Connected!")};
+    mySocket.onerror = function(msg){alert("FUCK")};
+    
+    
+}
