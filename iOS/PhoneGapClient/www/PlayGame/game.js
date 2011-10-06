0832ddba8ac9abdd6345dc6b1f5db756d7c7424e
Josef Lange <josef.d.lange@gmail.com>
2011-10-06 16:46:51 -0700

    Worked more on getting PhoneGap to Socket itself.

--- iOS/PhoneGapClient/www/PlayGame/game.js
+++ iOS/PhoneGapClient/www/PlayGame/game.js
@@ -1,14 +1,21 @@
  function promptLogin(){
 		var name = prompt("Hey you!"); //"Username please. (Use \"guest\" if you don't already have an account.");
 		//var pass = prompt("Please enter your password. (If you are logging in as \"guest\" then please use \"pass\".)");
-        
  }
 
 function connectToGame() {
+  
+    navigator.notification.alert(
+                                 'You are the winner!',  // message
+                                 alertDismissed,         // callback
+                                 'Game Over',            // title
+                                 'Done'                  // buttonName
+                                 );
     
-    var mySocket = new GapSocket(cs340-serv, 8000);
-    mySocket.onopen = function(){alert("Connected!")};
-    mySocket.onerror = function(msg){alert("FUCK")};
+}
+
+function alertDismissed() {
     
+    // Whatever.
     
 }
