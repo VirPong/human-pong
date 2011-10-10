4fca8040de4359bde1e443726583c6a0ecb2f5bc
Jordan Apele <jordan.apele@gmail.com>
2011-10-09 22:49:11 -0700

    Bluetooth plugin test

---
+++ Android/BluetoothPlugin/src/com/android/BluetoothPlugin/BluetoothPluginActivity.java
@@ -0,0 +1,14 @@
+package com.android.BluetoothPlugin;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class BluetoothPluginActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
